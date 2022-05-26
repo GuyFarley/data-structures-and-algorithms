e1708fbffd2802785de20ddb32aed532b8f75c8c
Guy Farley <guy.e.farley@gmail.com>
2022-05-25 20:18:43 -0700

    Completed 1 challenge so far

--- javascript/code-challenges/challenges-12.test.js
+++ javascript/code-challenges/challenges-12.test.js
@@ -43,11 +43,8 @@ If the word is between 5 and 10 characters long, return true. Otherwise, return 
 const validateWord = (word) => {
   // Solution code here...
   let regex = /[a-zA-Z]{5,10}/
-  if (regex.test(word)) {
-    return true;
-  } else if (regex.test(word) === false) {
-    return false;
-  }
+  return regex.test(word);
+
 };
 
 /* ------------------------------------------------------------------------------------------------
